251cba33a3c8ab2f2e3f46fe8408ab8d8bb8acfa
woquan99 <1092999319@qq.com>
2022-06-20 08:40:01 +0800

    2022/6/20 08:39

--- src/Shangguigu/oop/up/MethodArgsTest.java
+++ src/Shangguigu/oop/up/MethodArgsTest.java
@@ -1,16 +1,23 @@
 package Shangguigu.oop.up;
 
 /**
- * 可变个数形参的方法：数据类型 ... 变量名
- * 当调用可变个数形参的方法时，传入的参数个数是：0个，1个，2个。。。
- * 可变个数形参的方法与本类中方法名相同，形参类型也相同的数组之间不构成重载，即无法共存（下面注释调的就是例子）
- *
- * */
+ * 可变个数形参的方法
+ * 1.jdk 5.0新增的内容
+ * 2.具体使用：
+ * 	2.1 可变个数形参的格式：数据类型 ... 变量名
+ * 	2.2 当调用可变个数形参的方法时，传入的参数的个数可以是：0个，1个，2个...
+ * 	2.3可变个数形参的方法与本类中方法名相同，形参不同的方法之间构成重载。
+ *  2.4可变个数形参的方法与本类中方法名相同，形参类型也相同的数组之间不构成重载。即二者不可共存。
+ *  2.5可变个数形参在方法中的形参中,必须声明在末尾。
+ *  2.6可变个数形参在方法中的形参中，最多只能声明一个可变形参。
+ */
+
 
 public class MethodArgsTest {
     public static void main(String[] args){
         MethodArgsTest test = new MethodArgsTest();
         test.show("hello");
+        test.show("hello","world");
     }
     public void show(String s){
         System.out.println("show(String)");
