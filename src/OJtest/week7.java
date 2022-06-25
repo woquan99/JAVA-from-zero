c97451abb06a7ff4271217bec7d4901b55faf85e
woquan99 <1092999319@qq.com>
2022-06-25 22:25:50 +0800

    2022/6/25 22:47

--- src/OJtest/week7.java
+++ src/OJtest/week7.java
@@ -9,9 +9,33 @@ import java.util.Scanner;
  * */
 public class week7 {
     public static void main(String[] args){
-//        Scanner input = new Scanner(System.in);
-//        String a = input.next();
-//        char[]
-//        System.out.println(k);
+        Scanner input = new Scanner(System.in);
+        String a = input.next();
+
+        // 方法一：
+        StringBuilder stringBuilder = new StringBuilder(a);
+        stringBuilder.reverse();
+        String s1 = stringBuilder.toString();
+        System.out.println(s1);
+
+        // 方法二：
+        String b = "";
+        for(int i=a.length()-1;i>=0;i--){ // 长度为【0，a.length-1】
+            b += a.charAt(i);
+        }
+        System.out.println(b);
+
+        // 方法三：
+        // 字符串转化为字符数组
+        char[] charArray = a.toCharArray();
+        char[] charArray1 = new char[a.length()];
+        for(int i=0;i<charArray.length;i++){
+            charArray1[i] = charArray[charArray.length-i-1];
+        }
+        //字符数组转化为字符串
+        String c = new String(charArray1);
+        System.out.println(c);
+
+
     }
 }
