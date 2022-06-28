9361b1a1f725fdcb1bce4fdf60b11a90d5a9cae4
woquan99 <1092999319@qq.com>
2022-06-28 20:17:03 +0800

    2022/6/28 20:16

---
+++ src/Shangguigu/oop/middle/exer1/Kids.java
@@ -0,0 +1,23 @@
+package Shangguigu.oop.middle.exer1;
+
+
+public class Kids extends ManKind{
+    private int yearsOld;
+    public Kids(){}
+    public Kids(int yearsOld){
+        this.yearsOld=yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld){
+        this.yearsOld=yearsOld;
+    }
+    public int getYearsOld(){
+        return yearsOld;
+    }
+    public void printAge(){
+        System.out.println("I am " + yearsOld);
+    }
+
+
+
+}
---
+++ src/Shangguigu/oop/middle/exer1/KidsTest.java
@@ -0,0 +1,17 @@
+package Shangguigu.oop.middle.exer1;
+
+public class KidsTest {
+    public static void main(String[] args){
+        Kids someone = new Kids(12);
+        someone.printAge();
+        someone.setYearsOld(18);
+        someone.setSalary(10);
+        someone.setSex(1);
+        someone.employeed();
+        someone.manOrWoman();
+
+
+
+
+    }
+}
