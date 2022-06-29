57d274059e2fcfbd81179cb9efd7d27fbe99235b
woquan99 <1092999319@qq.com>
2022-06-29 08:23:43 +0800

    2022/6/29 08:23

--- src/Shangguigu/oop/middle/exer1/Kids.java
+++ src/Shangguigu/oop/middle/exer1/Kids.java
@@ -18,6 +18,11 @@ public class Kids extends ManKind{
         System.out.println("I am " + yearsOld);
     }
 
-
+    // 在Kids中重新定义employeed()方法，
+    // 覆盖父类ManKind中定义的employeed()方法，
+    // 输出“Kids should study and no job.”
+    public void employeed(){
+        System.out.println("Kids should study and no job.");
+    }
 
 }
