17b2c99c81e40cd418d7bb13f85e92227dde517e
woquan99 <1092999319@qq.com>
2022-07-08 00:16:13 +0800

    2022/7/8 0：16

---
+++ src/OJtest/week11/test1.java
@@ -0,0 +1,28 @@
+package OJtest.week11;
+
+import java.util.Scanner;
+
+/**
+ * 题目来源：https://matiji.net/exam/dohomework/1081/1
+ * 给定线段s1，s2，求解两线段之间的最短距离（小数点后保留8位小数）。线段s1：两端点p1,p2，线段s2:两端点p3，p4。
+ * 格式
+ * m，
+ * 输入格式：
+ * 第1次查询
+ * 第2次查询
+ * 第m次查询
+ * 每次查询格式如下：
+ *  Xp1Yp1Xp2Yp2Xp3Yp3Xp4Yp4
+ * 输出格式：共m行，对每个询问输出一行，即两线段之间的最短距离（小数点后保留8位小数）
+ *
+ *
+ */
+
+
+public class test1 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+
+    }
+}
