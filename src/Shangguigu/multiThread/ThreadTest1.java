af4d37d86c7b6c3d5ebb052c441b2164d6e18765
woquan99 <1092999319@qq.com>
2022-07-14 15:01:15 +0800

    2022/7/14 15:00

--- src/Shangguigu/multiThread/ThreadTest1.java
+++ src/Shangguigu/multiThread/ThreadTest1.java
@@ -9,7 +9,20 @@ package Shangguigu.multiThread;
  * 4.将此对象作为参数传递到 Thread 类的构造器中，创建Thread类的对象
  * 5.通过 Thread 类的对象调用 start()
  */
-//1.创建一个实现了Runnable接口得类
+
+/**
+ *  比较创建线程的两种方式。
+ *  开发中：优先选择：实现 Runnable 接口的方式
+ *  原因：1. 实现的方式没有类的单继承性的局限性
+ *       2. 实现的方式更适合来处理多个线程有共享数据的情况。
+ *
+ *  联系：public class Thread implements Runnable
+ *  相同点：两种方式都需要重写run(),将线程要执行的逻辑声明在run()中。
+ */
+
+
+//1.创建一个实现了 Runnable 接口的类
+//    方式一是：class MyThread extends Thread{}
 class MThread implements Runnable{
 
     //2.实现类去实现Runnable中的抽象方法:run()
@@ -30,6 +43,7 @@ public class ThreadTest1 {
         //4.将此对象作为参数传递到Thread类的构造器中，创建Thread类的对象
         Thread t1 = new Thread(m1);
         //5.通过Thread类的对象调用start():①启动线程 ②调用当前线程的run() --> 调用了Runnable类型的target的run()
+        t1.setName("线程1");
         t1.start();
 
         //再启动一个线程，遍历100以内的偶数
