49476353de3b3210c3189c19fa744abf106e79df
woquan99 <1092999319@qq.com>
2022-07-14 14:22:30 +0800

    2022/7/14 14:22

--- src/Shangguigu/multiThread/ThreadModeTest.java
+++ src/Shangguigu/multiThread/ThreadModeTest.java
@@ -16,19 +16,34 @@ package Shangguigu.multiThread;
  * 10.isAlive()：返回boolean，判断线程是否还活着
  */
 
+/**
+ * - 线程的优先级等级
+ *   - MAX_PRIORITY：10
+ *   - MIN _PRIORITY：1
+ *   - NORM_PRIORITY：5 --->默认优先级
+ * - 涉及的方法
+ *   - getPriority() ：返回线程优先值
+ *   - setPriority(int newPriority) ：改变线程的优先级
+ *
+ *   说明:高优先级的线程要抢占低优先级线程 cpu 的执行权。
+ *       但是只是从概率上讲，高优先级的线程高概率的情况下被执行。
+ *       并不意味着只有当高优先级的线程执行完以后，低优先级的线程才会被执行。
+ */
+
+
 class HelloThread extends Thread{
     @Override
     public void run() {
         for(int i = 0;i < 100; i++){
 
-            try {
-                sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//            try {
+//                sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
 
             if(i % 2 == 0){
-                System.out.println(Thread.currentThread().getName() + ":" + i);
+                System.out.println(getName() + ":" + getPriority()+":"+i);
             }
 //            if(i % 20 == 0){
 //                yield();
@@ -45,27 +60,34 @@ class HelloThread extends Thread{
 
 public class ThreadModeTest {
     public static void main(String[] args) {
-        HelloThread h1 = new HelloThread("Thread : 1");
-        h1.setName("线程一");// 设置线程的名字
-        h1.start();
+//        HelloThread h1 = new HelloThread("Thread : 1");
+//        h1.setName("线程一");// 设置线程的名字
+//        h1.start();
+        HelloThread h2 = new HelloThread("Thread : 1");
+        h2.setName("线程二");// 设置线程的名字
+        h2.start();
+        h2.setPriority(Thread.MAX_PRIORITY);
 
         //给主线程命名
         Thread.currentThread().setName("主线程");
+        Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 
         for(int i = 0;i < 100; i++){
             if(i % 2 == 0){
-                System.out.println(Thread.currentThread().getName() + ":" + i);
+                System.out.println(Thread.currentThread().getName() + ":" + Thread.currentThread().getPriority()+":"+i);
             }
 
-            if(i == 20){
-                try {
-                    h1.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+//            if(i == 20){
+//                try {
+//                    h1.join();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
         }
 
-        System.out.println(h1.isAlive());
+//        System.out.println(h1.isAlive());
+        // 此时线程 h1 已经完全执行完毕
+        System.out.println(h2.isAlive());
     }
 }
