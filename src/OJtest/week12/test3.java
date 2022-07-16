254d2a3c3f9c0403cb7a596e51c95ab157d36394
woquan99 <1092999319@qq.com>
2022-07-16 22:19:02 +0800

    2022/7/16 22:18

---
+++ src/OJtest/week12/test3.java
@@ -0,0 +1,29 @@
+package OJtest.week12;
+
+import java.util.Scanner;
+
+/**
+ * https://matiji.net/exam/dohomework/1186/3
+ *
+ */
+
+public class test3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String[] a = new String[n];
+        for(int i=0;i<a.length;i++){
+            a[i] = input.next();
+        }
+        int m = input.nextInt();
+        String[] b = new String[m];
+        for(int j=0;j<b.length;j++){
+            b[j] = input.next();
+        }
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]=a[])
+        }
+        System.out.println(m);
+
+    }
+}
