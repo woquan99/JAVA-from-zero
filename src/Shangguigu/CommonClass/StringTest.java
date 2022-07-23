805b45050ab75ea445718f3dd769c8ef08c804d9
woquan99 <1092999319@qq.com>
2022-07-23 23:36:13 +0800

    2022/7/23 23:12

--- src/Shangguigu/CommonClass/StringTest.java
+++ src/Shangguigu/CommonClass/StringTest.java
@@ -48,6 +48,58 @@ public class StringTest {
     }
 
 
+    /**
+     * String的实例化方式
+     * 方式一：通过字面量定义的方式
+     * 方式二：通过new + 构造器的方式
+     *
+     * 面试题：String s = new String("abc");方式创建对象，在内存中创建了几个对象？
+     *      两个:一个是堆空间中 new 结构，另一个是char[]对应的常量池中的数据："abc"
+     *
+     */
+    @Test
+    public void test2(){
+        //通过字面量定义的方式：此时的s1和s2的数据javaEE声明在方法区中的字符串常量池中。
+        String s1 = "javaEE";
+        String s2 = "javaEE";
+
+        //通过new + 构造器的方式:此时的s3和s4保存的地址值，是数据在堆空间中开辟空间以后对应的地址值。
+        String s3 = new String("javaEE");
+        String s4 = new String("javaEE");
+
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s3);//false
+        System.out.println(s1 == s4);//false
+        System.out.println(s3 == s4);//false
+
+        System.out.println("***********************");
+        Person p1 = new Person("Tom",12);
+        Person p2 = new Person("Tom",12);
+
+        System.out.println(p1.name.equals(p2.name));//true
+        System.out.println(p1.name == p2.name);//true
+
+        p1.name = "Jerry";
+        System.out.println(p2.name);//Tom
+    }
 
 
 }
+/**
+ * @author subei
+ * @create 2020-05-09 11:20
+ */
+class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+}
+
