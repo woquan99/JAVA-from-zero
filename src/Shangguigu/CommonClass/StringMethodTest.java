5e7ccf4085d777af3c4ac43d5309ab80cd2e9fdb
woquan99 <1092999319@qq.com>
2022-07-24 14:43:30 +0800

    2022/7/24 14:45

--- src/Shangguigu/CommonClass/StringMethodTest.java
+++ src/Shangguigu/CommonClass/StringMethodTest.java
@@ -2,6 +2,8 @@ package Shangguigu.CommonClass;
 
 import org.junit.Test;
 
+import java.util.Locale;
+
 public class StringMethodTest {
 
     /**
@@ -12,7 +14,7 @@ public class StringMethodTest {
      * String toUpperCase()：使用默认语言环境，将String中的所有字符转换为大写
      * String trim()：返回字符串的副本，忽略前导空白和尾部空白
      * boolean equals(Object obj)：比较字符串的内容是否相同
-     * boolean equals IgnoreCase(String anotherString)：与equals方法类似，忽略大小写
+     * boolean equals IgnoreCase(String anotherString)：与equals方法类似，忽略大小写(输密码比较时可用)
      * String concat(String str)：将指定字符串连接到此字符串的结尾。等价于用“+”
      * int compareTo(String anotherString)：比较两个字符串的大小
      * String substring(int beginIndex)：返回一个新的字符串，它是此字符串的从beginIndex开始截取到最后的一个子字符串。
@@ -20,16 +22,16 @@ public class StringMethodTest {
      */
 
     @Test
-    public void Test1(){
+    public void Test1() {
         String s1 = "helloworld";
         System.out.println(s1.length());
         System.out.println(s1.charAt(0));
         System.out.println(s1.charAt(9));
-        System.out.println(s1.charAt(10));
+//        System.out.println(s1.charAt(10));
 //        s1 = "";
         System.out.println(s1.isEmpty());
 
-        String s2 = s1.toLowerCase();
+        String s2 = s1.toUpperCase();
         System.out.println(s1);//s1不可变的，仍然为原来的字符串
         System.out.println(s2);//改成小写以后的字符串
 
@@ -40,7 +42,7 @@ public class StringMethodTest {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         String s1 = "HelloWorld";
         String s2 = "helloworld";
         System.out.println(s1.equals(s2));//false
@@ -63,4 +65,46 @@ public class StringMethodTest {
         System.out.println(s9);
     }
 
+
+    /**
+     * boolean endsWith(String suffix)：测试此字符串是否以指定的后缀结束
+     * boolean startsWith(String prefix)：测试此字符串是否以指定的前缀开始
+     * boolean startsWith(String prefix, int toffset)：测试此字符串从指定索引开始的子字符串是否以指定前缀开始
+     * <p>
+     * boolean contains(CharSequence s)：当且仅当此字符串包含指定的 char 值序列时，返回 true
+     * int indexOf(String str)：返回指定子字符串在此字符串中第一次出现处的索引
+     * int indexOf(String str, int fromIndex)：返回指定子字符串在此字符串中第一次出现处的索引，从指定的索引开始
+     * int lastIndexOf(String str)：返回指定子字符串在此字符串中最右边出现处的索引
+     * int lastIndexOf(String str, int fromIndex)：返回指定子字符串在此字符串中最后一次出现处的索引，从指定的索引开始反向搜索
+     * <p>
+     * 注：indexOf和lastIndexOf方法如果未找到都是返回-1
+     */
+    @Test
+    public void test3() {
+        String str1 = "helloworld";
+        boolean b1 = str1.endsWith("rld");
+        System.out.println(b1);
+
+        boolean b2 = str1.startsWith("He");
+        System.out.println(b2);
+
+        boolean b3 = str1.startsWith("ll", 2);
+        System.out.println(b3);
+
+        String str2 = "wor";
+        System.out.println(str1.contains(str2));
+
+        System.out.println(str1.indexOf("lo"));
+
+        System.out.println(str1.indexOf("lo", 5));
+
+        String str3 = "hellorworld";
+
+        System.out.println(str3.lastIndexOf("or"));
+        System.out.println(str3.lastIndexOf("or", 6));
+
+        //什么情况下，indexOf(str)和lastIndexOf(str)返回值相同？
+        //情况一：存在唯一的一个str。情况二：不存在str
+    }
 }
+
