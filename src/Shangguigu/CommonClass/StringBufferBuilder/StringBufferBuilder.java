ca06f6a48b96d778730963506cc964cc3490bd00
woquan99 <1092999319@qq.com>
2022-07-25 15:11:47 +0800

    2022/7/25 15:11

--- src/Shangguigu/CommonClass/StringBufferBuilder/StringBufferBuilder.java
+++ src/Shangguigu/CommonClass/StringBufferBuilder/StringBufferBuilder.java
@@ -86,5 +86,43 @@ public class StringBufferBuilder{
         System.out.println(s2);
     }
 
+
+    /**
+     * 对比String、StringBuffer、StringBuilder三者的效率：
+     * 从高到低排列：StringBuilder > StringBuffer > String
+     *
+     */
+    @Test
+    public void test3(){
+        //初始设置
+        long startTime = 0L;
+        long endTime = 0L;
+        String text = "";
+        StringBuffer buffer = new StringBuffer("");
+        StringBuilder builder = new StringBuilder("");
+        //开始对比
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            buffer.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuffer的执行时间：" + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            builder.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuilder的执行时间：" + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            text = text + i;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("String的执行时间：" + (endTime - startTime));
+
+    }
+
 }
 
