3f3f1d87e80db7ec4e5fdf88ff20d950e943f44d
woquan99 <1092999319@qq.com>
2022-07-27 15:35:07 +0800

    2022/7/27 15:35

---
+++ src/Shangguigu/EnumAndNote/EnumTest.java
@@ -0,0 +1,58 @@
+package Shangguigu.EnumAndNote;
+
+/**
+ * 一、枚举类的使用
+ * 1.枚举类的理解：类的对象只有有限个，确定的。我们称此类为枚举类。
+ * 2.当需要定义一组常量时，强烈建议使用枚举类
+ * 3.若枚举只有一个对象, 则可以作为一种单例模式的实现方式。
+ *
+ * 二、如何定义枚举类
+ *     方式一：JDK1.5之前需要自定义枚举类
+ *     方式二：JDK 1.5 新增的enum 关键字用于定义枚举类
+ *
+ */
+public class EnumTest {
+    public static void main(String[] args) {
+        Season spring = Season.SPRING;
+        Season summer = Season.SUMMER;
+        System.out.println(spring);
+        System.out.println(summer);
+    }
+}
+
+// 自定义枚举类
+class Season{
+    //1.声明Season对象的属性:private final修饰
+    private final String seasonName;
+    private final String seasonDesc;
+
+    //2.私有化类的构造器,并给对象属性赋值
+    private Season(String seasonName,String seasonDesc){
+        this.seasonName = seasonName;
+        this.seasonDesc = seasonDesc;
+    }
+
+    //3.提供当前枚举类的多个对象
+    public static final Season SPRING = new Season("春天","万物复苏");
+    public static final Season SUMMER = new Season("夏天","烈日炎炎");
+    public static final Season AUTUMN = new Season("秋天","金秋送爽");
+    public static final Season WINTER = new Season("冬天","白雪皑皑");
+
+    //4.其他诉求：获取枚举类对象的属性
+    public String getSeasonName() {
+        return seasonName;
+    }
+
+    public String getSeasonDesc() {
+        return seasonDesc;
+    }
+
+    //4.其他诉求1：提供toString()
+    @Override
+    public String toString() {
+        return "Season{" +
+                "seasonName='" + seasonName + '\'' +
+                ", seasonDesc='" + seasonDesc + '\'' +
+                '}';
+    }
+}
