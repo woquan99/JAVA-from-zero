242994ac888fdffec19df4895236589b954c18b6
woquan99 <1092999319@qq.com>
2022-07-30 09:16:04 +0800

    2022/7/30 09:20

--- src/Shangguigu/NetworkProgramming/URLTest1.java
+++ src/Shangguigu/NetworkProgramming/URLTest1.java
@@ -6,6 +6,13 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * URI 是uniform resource identifier，统一资源标识符，用来唯一的标识一个资源。
+ * URL 是uniform resource locator，统一资源定位符，它是一种具体的URI，即URL可以用来标识一个资源，而且还指明了如何locate这个资源。
+ * URN 是uniform resource name，统一资源命名，是通过名字来标识资源，比如mailto:java-net@java.sun.com。
+ * 也就是说，URI是以一种抽象的，高层次概念定义统一资源标识，而URL和URN则是具体的资源标识的方式。URL和URN都是一种URI。
+ */
+
 public class URLTest1 {
     public static void main(String[] args) {
         HttpURLConnection urlConnection = null;
