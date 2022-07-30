0cef2821600ef849083f425e943960b514b3ee87
woquan99 <1092999319@qq.com>
2022-07-30 16:49:07 +0800

    2022/7/30 16:48

---
+++ src/Shangguigu/IOStream/FileReaderWriterTest.java
@@ -0,0 +1,75 @@
+package Shangguigu.IOStream;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * Java程序中，对于数据的输入/输出操作以“流(stream)”的方式进行。
+ * java.io包下提供了各种“流”类和接口，用以获取不同种类的数据，并通过标准的方法输入或输出数据。
+ * 输入input：读取外部数据（磁盘、光盘等存储设备的数据）到程序（内存）中。
+ * 输出output：将程序（内存）数据输出到磁盘、光盘等存储设备中。
+ *
+ * 一、流的分类：
+ * 1.操作数据单位：字节流(InputStream/OutputStream)、字符流(Reader/Writer)
+ * 2.数据的流向：输入流、输出流
+ * 3.流的角色：节点流、处理流
+ *
+ * 二、流的体系结构
+ * 抽象基类         节点流（或文件流）                                  缓冲流（处理流的一种）
+ * InputStream     FileInputStream   (read(byte[] buffer))        BufferedInputStream (read(byte[] buffer))
+ * OutputStream    FileOutputStream  (write(byte[] buffer,0,len)  BufferedOutputStream (write(byte[] buffer,0,len) / flush()
+ * Reader          FileReader (read(char[] cbuf))                 BufferedReader (read(char[] cbuf) / readLine())
+ * Writer          FileWriter (write(char[] cbuf,0,len)           BufferedWriter (write(char[] cbuf,0,len) / flush()
+ */
+public class FileReaderWriterTest {
+    public static void main(String[] args) {
+        File file = new File("F:\\IDEA Data\\src\\hi.txt");//相较于当前工程
+        System.out.println(file.getAbsolutePath());
+
+        File file1 = new File("F:\\IDEA Data\\src\\hello1.txt");
+        System.out.println(file1.getAbsolutePath());
+    }
+
+    /**
+     * hello1.txt文件内容读入程序中，并输出到控制台
+     *
+     * 说明点：
+     *     1. read()的理解：返回读入的一个字符。如果达到文件末尾，返回-1
+     *     2. 异常的处理：为了保证流资源一定可以执行关闭操作。需要使用try-catch-finally处理
+     *     3. 读入的文件一定要存在，否则就会报 FileNotFoundException。
+     *
+     */
+    @Test
+    public void test(){
+        FileReader fr = null;
+        try {
+            //实例化File对象，指明要操作的文件
+            File file = new File("F:\\IDEA Data\\src\\hello1.txt");//相较于当前的Module
+            //2.提供具体的流
+            fr = new FileReader(file);
+
+            //3.数据的读入过程
+            //read():返回读入的一个字符。如果达到文件末尾，返回-1.
+            int data;
+            while((data = fr.read()) != -1){
+                System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //4.流的关闭操作
+            if(fr != null){
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
