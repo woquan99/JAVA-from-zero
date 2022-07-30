3f28cca8fb8856d7c06e964872f32e7da6a49125
woquan99 <1092999319@qq.com>
2022-07-30 14:41:05 +0800

    2022/7/30 14:40

--- src/Shangguigu/IOStream/FileExer/FindJPGFileTest.java
+++ src/Shangguigu/IOStream/FileExer/FindJPGFileTest.java
@@ -3,6 +3,7 @@ package Shangguigu.IOStream.FileExer;
 import org.junit.Test;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 /**
  * 2.判断指定目录下是否有后缀名为.jpg的文件，如果有，就输出该文件名称
@@ -11,7 +12,7 @@ import java.io.FileFilter;
 public class FindJPGFileTest {
     @Test
     public void test(){
-        File srcfile  = new File("F:\\IDEA Data\\src\\Shangguigu");
+        File srcfile  = new File("C:\\Users\\偓佺\\Pictures\\Camera Roll");
         String[] filenames = srcfile.list();
         for(String filename : filenames){
             if(filename.endsWith(".jpg")){
@@ -22,7 +23,7 @@ public class FindJPGFileTest {
 
     @Test
     public void test2(){
-        File srcfile  = new File("F:\\IDEA Data\\src\\Shangguigu");
+        File srcfile  = new File("C:\\Users\\偓佺\\Pictures\\Camera Roll");
         File[] listfiles = srcfile.listFiles();
         for(File file:listfiles){
             if(file.getName().endsWith(".jpg")){
@@ -31,5 +32,27 @@ public class FindJPGFileTest {
         }
     }
 
+
+    /**
+     * File类提供了两个文件过滤器方法
+     * public String[] list(FilenameFilter filter)
+     * public File[] listFiles(FileFilter filter)
+     */
+    @Test
+    public void test3(){
+        File srcFile = new File("C:\\Users\\偓佺\\Pictures\\Camera Roll");
+
+        File[] subFiles = srcFile.listFiles(new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".jpg");
+            }
+        });
+
+        for(File file : subFiles){
+            System.out.println(file.getAbsolutePath());
+        }
+    }
 
 }
