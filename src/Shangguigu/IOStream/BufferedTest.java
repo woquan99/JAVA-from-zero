7df716af6e91a1feaf1fd15f49de707fdd3f6917
woquan99 <1092999319@qq.com>
2022-08-01 15:44:02 +0800

    2022/8/1 15:40

--- src/Shangguigu/IOStream/BufferedTest.java
+++ src/Shangguigu/IOStream/BufferedTest.java
@@ -141,5 +141,61 @@ public class BufferedTest {
         System.out.println("复制操作花费的时间为：" + (end - start));//1
     }
 
+
+
+    /**
+     * 使用BufferedReader和BufferedWriter实现文本文件的复制
+     */
+    @Test
+    public void test4(){
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try {
+            //创建文件和相应的流
+            br = new BufferedReader(new FileReader(new File("F:\\IDEA Data\\src\\hello.txt")));
+            bw = new BufferedWriter(new FileWriter(new File("F:\\IDEA Data\\src\\helloBufferedReader.txt")));
+
+            //读写操作
+            //方式一：使用char[]数组
+            char[] cbuf = new char[1024];
+            int len;
+            while((len = br.read(cbuf)) != -1){
+                bw.write(cbuf,0,len);
+    //            bw.flush();
+            }
+
+            //方式二：使用String
+//            String data;
+//            while((data = br.readLine()) != null){
+//                //方法一：
+////                bw.write(data + "\n");//data中不包含换行符
+//                //方法二：
+//                bw.write(data);//data中不包含换行符
+//                bw.newLine();//提供换行的操作
+//            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            if(bw != null){
+
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(br != null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
 
