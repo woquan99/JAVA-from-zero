0b3ee7efe8d2cb421c83f847c8058287649b71d3
woquan99 <1092999319@qq.com>
2022-08-01 09:46:57 +0800

    2022/8/1 09:45

--- src/Shangguigu/IOStream/FileIOPutTest.java
+++ src/Shangguigu/IOStream/FileIOPutTest.java
@@ -93,6 +93,69 @@ public class FileIOPutTest {
 
             }
         }
-
     }
+
+
+
+    // 指定路径下文件的复制
+    public void copyFile(String srcPath,String destPath){
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            //
+            File srcFile = new File(srcPath);
+            File destFile = new File(destPath);
+
+            //
+            fis = new FileInputStream(srcFile);
+            fos = new FileOutputStream(destFile);
+
+            //复制的过程
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = fis.read(buffer)) != -1){
+                fos.write(buffer,0,len);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null){
+                //
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void testCopyFile(){
+
+        long start = System.currentTimeMillis();
+
+//        String srcPath = "C:\\Users\\29433\\Desktop\\164.jpg";
+//        String destPath = "C:\\Users\\29433\\Desktop\\164.jpg";
+
+        String srcPath = "F:\\IDEA Data\\src\\hello1.txt";
+        String destPath = "F:\\IDEA Data\\src\\hello.txt";
+
+        copyFile(srcPath,destPath);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("复制操作花费的时间为：" + (end - start));//1
+    }
+
+
 }
