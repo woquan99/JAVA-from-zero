c1a0e6e3994546f1f96844eb8062384111002a8c
woquan99 <1092999319@qq.com>
2022-08-01 09:43:12 +0800

    2022/8/1 09:43

--- src/Shangguigu/IOStream/FileIOPutTest.java
+++ src/Shangguigu/IOStream/FileIOPutTest.java
@@ -3,6 +3,7 @@ package Shangguigu.IOStream;
 import org.junit.Test;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -44,5 +45,54 @@ public class FileIOPutTest {
                 }
             }
         }
+    }
+
+
+    /**
+     * 实现对图片的复制操作
+     */
+    @Test
+    public void testFileInputOutputStream(){
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            // 1.造文件
+            File srcFile = new File("F:\\IDEA Data\\src\\pikaqiu.jpg");
+            File destFile = new File("F:\\IDEA Data\\src\\pikaqiuCopy.jpg");
+
+            // 2.造流
+            fis = new FileInputStream(srcFile);
+            fos = new FileOutputStream(destFile);
+
+            // 3.复制的过程
+            byte[] buffer = new byte[5];
+            int len;
+            // 4.读数据
+            while((len= fis.read(buffer))!=-1){
+                fos.write(buffer,0,len);
+            }
+            System.out.println("复制成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null){
+                //5.关闭资源
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
     }
 }
