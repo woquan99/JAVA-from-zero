84e849ebb2ba5901321e6d5891fe306d7d8dd29c
woquan99 <1092999319@qq.com>
2022-08-01 09:27:10 +0800

    2022/8/1 09:28

--- src/Shangguigu/IOStream/FileReaderWriterTest.java
+++ src/Shangguigu/IOStream/FileReaderWriterTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -126,5 +127,104 @@ public class FileReaderWriterTest {
 
     }
 
+    /**
+     * 从内存中写出数据到硬盘的文件里。
+     *
+     * 说明：
+     * 1.输出操作，对应的File可以不存在的。并不会报异常
+     * 2.
+     *   File对应的硬盘中的文件如果不存在，在输出的过程中，会自动创建此文件。
+     *   File对应的硬盘中的文件如果存在：
+     *       如果流使用的构造器是：FileWriter(file,false) / FileWriter(file):对原有文件的覆盖
+     *       如果流使用的构造器是：FileWriter(file,true):不会对原有文件覆盖，而是在原有文件基础上追加内容
+     */
+    @Test
+    public void test3(){
+        FileWriter fw = null;
+        try {
+            //1.提供File类的对象，指明写出到的文件
+            File file = new File("F:\\IDEA Data\\src\\hi.txt");
+
+            //2.提供FileWriter的对象，用于数据的写出
+            fw = new FileWriter(file,false);
+
+            //3.写出的操作
+            fw.write("I have a dream!\n");
+            fw.write("you need to have a dream!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.流资源的关闭
+            if(fw != null){
+
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    @Test
+    public void test4() {
+        FileReader fr = null;
+        FileWriter fw = null;
+        try {
+            //1.创建File类的对象，指明读入和写出的文件
+            File srcFile = new File("F:\\IDEA Data\\src\\hello1.txt");
+            File srcFile2 = new File("F:\\IDEA Data\\src\\hi.txt");
+
+            //不能使用字符流来处理图片等字节数据
+//            File srcFile = new File("爱情与友情.jpg");
+//            File srcFile2 = new File("爱情与友情1.jpg");
+
+            //2.创建输入流和输出流的对象
+            fr = new FileReader(srcFile);
+            fw = new FileWriter(srcFile2);
+
+            //3.数据的读入和写出操作
+            char[] cbuf = new char[5];
+            int len;//记录每次读入到cbuf数组中的字符的个数
+            while((len = fr.read(cbuf)) != -1){
+                //每次写出len个字符
+                fw.write(cbuf,0,len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.关闭流资源
+            //方式一：
+//            try {
+//                if(fw != null)
+//                    fw.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }finally{
+//                try {
+//                    if(fr != null)
+//                        fr.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+            //方式二：
+            try {
+                if(fw != null)
+                    fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if(fr != null)
+                    fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
