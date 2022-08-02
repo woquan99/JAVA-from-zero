37ceaf4d6b01f255d3f24454f896b80241715bce
woquan99 <1092999319@qq.com>
2022-08-02 09:36:55 +0800

    2022/8/2 09:36

---
+++ src/Shangguigu/IOStream/Serializable/ObjectTest2.java
@@ -0,0 +1,190 @@
+package Shangguigu.IOStream.Serializable;
+
+import org.junit.Test;
+import java.io.*;
+
+/**
+ * Person需要满足如下的要求，方可序列化
+ * 1.需要实现接口：Serializable
+ * 2.当前类提供一个全局常量：serialVersionUID
+ * 3.除了当前Person类需要实现Serializable接口之外，还必须保证其内部所有属性
+ *   也必须是可序列化的。（默认情况下，基本数据类型可序列化）
+ *
+ *
+ * 补充：ObjectOutputStream和ObjectInputStream不能序列化static和transient修饰的成员变量
+ *
+ */
+class Person2 implements Serializable{
+
+    public static final long serialVersionUID = 475463534532L;
+
+    private String name;
+    private int age;
+    private int id;
+    private Account acct;
+
+    public Person2(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public Person2(String name, int age, int id, Account acct) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.acct = acct;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                ", acct=" + acct +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person2(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person2() {
+    }
+}
+
+class Account implements Serializable{
+    public static final long serialVersionUID = 4754534532L;
+    private double balance;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "balance=" + balance +
+                '}';
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Account(double balance) {
+
+        this.balance = balance;
+    }
+}
+
+
+/**
+ * 对象流的使用
+ * 1.ObjectInputStream 和 ObjectOutputStream
+ * 2.作用：用于存储和读取基本数据类型数据或对象的处理流。它的强大之处就是可以把Java中的对象写入到数据源中，也能把对象从数据源中还原回来。
+ *
+ * 3.要想一个java对象是可序列化的，需要满足相应的要求。见Person.java
+ *
+ * 4.序列化机制：
+ * 对象序列化机制允许把内存中的Java对象转换成平台无关的二进制流，从而允许把这种
+ * 二进制流持久地保存在磁盘上，或通过网络将这种二进制流传输到另一个网络节点。
+ * 当其它程序获取了这种二进制流，就可以恢复成原来的Java对象。
+ */
+public class ObjectTest2 {
+
+    /**
+     * 序列化过程：将内存中的java对象保存到磁盘中或通过网络传输出去
+     * 使用ObjectOutputStream实现
+     */
+    @Test
+    public void test(){
+        ObjectOutputStream oos = null;
+        try {
+            //创造流
+            oos = new ObjectOutputStream(new FileOutputStream("F:\\IDEA Data\\src\\ObjectTest2.dat"));
+            //制造对象
+            oos.writeObject(new String("Nankai University 第二次欢迎你"));
+            //刷新操作
+            oos.flush();
+
+            oos.writeObject(new Person2("李时珍",65));
+            oos.flush();
+
+            oos.writeObject(new Person2("张学良",23,1001,new Account(5000)));
+            oos.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(oos != null){
+                //3.关闭流
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 反序列化：将磁盘文件中的对象还原为内存中的一个java对象
+     * 使用ObjectInputStream来实现
+     */
+    @Test
+    public void test2(){
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("F:\\IDEA Data\\src\\ObjectTest2.dat"));
+
+            Object obj = ois.readObject();
+            String str = (String) obj;
+
+            Person2 p = (Person2) ois.readObject();
+
+            System.out.println(str);
+            System.out.println(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(ois != null){
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
