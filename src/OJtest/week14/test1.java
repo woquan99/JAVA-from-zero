7dd05c68c787de7ecb6c811f6b8936d9ef4f9e1d
woquan99 <1092999319@qq.com>
2022-08-04 09:34:31 +0800

    2022/8/4 09:30

---
+++ src/OJtest/week14/test1.java
@@ -0,0 +1,36 @@
+package OJtest.week14;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 刻俄柏在打怪，她的面前有n个怪，第i个怪的血量为ai。对于一个怪物，她有两种攻击方式：
+ * 1.使用ai次普通攻击消灭该怪物
+ * 2.施放一次技能消灭该怪物
+ * 刻俄柏最多只能使用k次技能，她想知道最少使用多少次普通攻击消灭所有怪物。
+ */
+public class test1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int n = input.nextInt();
+        int k = input.nextInt();
+        double[] arr = new double[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextDouble();
+        }
+        if (n <= k) {
+            System.out.print(0);
+        }
+        else {
+            Arrays.sort(arr);// 按照从小到大的顺序排列
+            for (int i = 0; i < (n - k); i++) {
+                sum += arr[i];
+//                int b = (int)arr[i]+1;
+//                sum += b;
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
