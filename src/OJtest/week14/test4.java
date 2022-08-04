ddfb1170b6486737a06e039deaf9f2201ca13888
woquan99 <1092999319@qq.com>
2022-08-04 11:44:02 +0800

    2022/8/4 11:45

--- src/OJtest/week14/test4.java
+++ src/OJtest/week14/test4.java
@@ -1,7 +1,14 @@
 package OJtest.week14;
 
+import java.util.Scanner;
+
 public class test4 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+
+
 
     }
 }
---
+++ src/OJtest/week14/test6.java
@@ -0,0 +1,20 @@
+package OJtest.week14;
+
+import java.util.Scanner;
+
+/**
+ * https://matiji.net/exam/dohomework/1389/6
+ * 现在奥利马船长为了加强红黄两种皮克敏的默契，决定让他们来团建。皮克敏们都会有一个座位，为了让不同颜色的皮克敏更熟悉，
+ * 对于任意连续一段的座位，红黄皮克敏的数目之差不能超过k。红皮克敏有X只，黄色的有y只，那么安排座位的总方案数有多少呢？
+ * 最终答案对10000007取模后输出。
+ */
+public class test6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int k = input.nextInt();
+
+
+    }
+}
