1d5d8fd295c62799da4e2b24f250aa8c384179eb
woquan99 <1092999319@qq.com>
2022-08-04 13:29:56 +0800

    2022/8/4 13:30

--- src/OJtest/week14/test4.java
+++ src/OJtest/week14/test4.java
@@ -8,11 +8,25 @@ import java.util.Scanner;
  */
 
 public class test4 {
+    public static int f(int n,int m)//定义n个球中取m个球
+    {
+        if(n<m)
+            return 0;//如果n小于m，则不存在
+        if(n==m)
+            return 1;        //只有一种取法
+        if(m==0)
+            return 1;             //只有一种取法
+        return f(n-1,m-1)+f(n-1,m);//有一个球设置为特殊球x，所有取法分成两部分，划分标准
+        //（是否包含x）
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         Long sum =0l;
-
+        for(int m=0;m<n;m++){
+            int k= f(n,m);
+        }
 
 
 
