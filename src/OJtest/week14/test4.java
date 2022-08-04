14356eb1d1677cc6e504fffb8196a58c9c12e46a
woquan99 <1092999319@qq.com>
2022-08-04 11:51:15 +0800

    2022/8/4 11:50

--- src/OJtest/week14/test4.java
+++ src/OJtest/week14/test4.java
@@ -2,10 +2,17 @@ package OJtest.week14;
 
 import java.util.Scanner;
 
+/**
+ * 从中取出个球的得分是小码哥取出k个球的所可能性的平方
+ * 组合C
+ */
+
 public class test4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        Long sum =0l;
+
 
 
 
