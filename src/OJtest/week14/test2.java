7360c11b850ddd2a7f1f090ef71c9d254f13a14f
woquan99 <1092999319@qq.com>
2022-08-04 11:11:59 +0800

    2022/8/4 11:15

--- src/OJtest/week14/test2.java
+++ src/OJtest/week14/test2.java
@@ -12,41 +12,42 @@ public class test2 {
         Scanner input = new Scanner(System.in);
         String s = input.next();
         System.out.print(-1);
-        // 最大长度
-        int max = 0;
-        // 起始字符
-        Character c = s.charAt(0);
-        // 当前数量
-        int num = 1;
-        // 起始下标
-        int start = 0;
-        // 最长子串
-        String maxStr = s.substring(0, 1);
-        // 遍历字符串
-        for (int i = 1; i < s.length(); i++) {
-            // 当前字符
-            char o = s.charAt(i);
-            // 判断是否相等
-            if (c.equals(o)) {
-                // 获取最长重复字符
-                if (++num > max) {
-                    maxStr = s.substring(start, i + 1);
-                }
-                // 获取最长重复字符的长度
-                max = Math.max(max, num);
-            } else {
-                c = o;
-                num = 1;
-                start = i;
-            }
-        }
-        // 不能放过只有一个的情况
-        if (num > max) {
-            maxStr = s.substring(start);
-        }
-        max = Math.max(max, num);
-
-    }
+//
+//        // 最大长度
+//        int max = 0;
+//        // 起始字符
+//        Character c = s.charAt(0);
+//        // 当前数量
+//        int num = 1;
+//        // 起始下标
+//        int start = 0;
+//        // 最长子串
+//        String maxStr = s.substring(0, 1);
+//        // 遍历字符串
+//        for (int i = 1; i < s.length(); i++) {
+//            // 当前字符
+//            char o = s.charAt(i);
+//            // 判断是否相等
+//            if (c.equals(o)) {
+//                // 获取最长重复字符
+//                if (++num > max) {
+//                    maxStr = s.substring(start, i + 1);
+//                }
+//                // 获取最长重复字符的长度
+//                max = Math.max(max, num);
+//            } else {
+//                c = o;
+//                num = 1;
+//                start = i;
+//            }
+//        }
+//        // 不能放过只有一个的情况
+//        if (num > max) {
+//            maxStr = s.substring(start);
+//        }
+//        max = Math.max(max, num);
+//
+//    }
 
 
     }
