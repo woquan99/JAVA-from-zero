8c145d41992ca51022bef53da2fee841376ffbfa
woquan99 <1092999319@qq.com>
2022-08-14 15:37:14 +0800

    2022/8/14 15:30

--- src/Shangguigu/Set/CollectionTest.java
+++ src/Shangguigu/Set/CollectionTest.java
@@ -34,10 +34,8 @@ package Shangguigu.Set;
  */
 
 import org.junit.Test;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
+
+import java.util.*;
 
 /**
  *
@@ -141,20 +139,57 @@ public class CollectionTest {
         coll.add(false);
 
         //5.retainAll(Collection coll1):交集：获取当前集合和coll1集合的交集，并返回给当前集合
-//        Collection coll1 = Arrays.asList(123,456,789);
-//        coll.retainAll(coll1);
-//        System.out.println(coll);
+        Collection coll1 = Arrays.asList(123,456,789);
+        coll.retainAll(coll1);
+        System.out.println(coll);
 
         //6.equals(Object obj):要想返回true，需要当前集合和形参集合的元素都相同。
-        Collection coll1 = new ArrayList();
-        coll1.add(456);
-        coll1.add(123);
-        coll1.add(new Person("Jerry",20));
-        coll1.add(new String("Tom"));
-        coll1.add(false);
-
-        System.out.println(coll.equals(coll1));
+        Collection coll2 = new ArrayList();
+        coll2.add(456);
+        coll2.add(123);
+        coll2.add(new Person("Jerry",20));
+        coll2.add(new String("Tom"));
+        coll2.add(false);
+        System.out.println(coll.equals(coll2));
     }
 
+
+    @Test
+    public void test4(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //7.hashCode():返回当前对象的哈希值
+        System.out.println(coll.hashCode());
+
+        //8.集合 --->数组：toArray()
+        Object[] arr = coll.toArray();
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        //拓展：数组 --->集合:调用Arrays类的静态方法asList()
+        List<String> list=Arrays.asList(new String[]{"aa","bb","cc"});
+        System.out.println(list);
+
+        List<int[]> list1=Arrays.asList(new int[]{123,53});
+        System.out.println(list1.size());// 认为是一个元素
+        System.out.println(list1);
+
+        List arr1 = Arrays.asList(123, 456);
+        System.out.println(arr1);//[123, 456]
+
+        List arr2 = Arrays.asList(new int[]{123, 456});
+        System.out.println(arr2.size());//1
+
+        List arr3 = Arrays.asList(new Integer[]{123, 456});
+        System.out.println(arr3.size());//2
+
+        //9.iterator():返回Iterator接口的实例，用于遍历集合元素。放在IteratorTest.java中测试
+    }
 
 }
---
+++ src/Shangguigu/Set/IteratorTest.java
@@ -0,0 +1,101 @@
+package Shangguigu.Set;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * 集合元素的遍历操作，使用迭代器Iterator接口
+ * 1.内部的方法：hasNext()和 next()
+ * 2.集合对象每次调用iterator()方法都得到一个全新的迭代器对象，默认游标都在集合的第一个元素之前。
+ * 3.内部定义了remove(),可以在遍历的时候，删除集合中的元素。此方法不同于集合直接调用remove()
+ */
+
+public class IteratorTest {
+
+    @Test
+    public void test(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        Iterator iterator = coll.iterator();
+
+        //方式一：
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.next());
+//        //报异常：NoSuchElementException
+//        //因为：在调用it.next()方法之前必须要调用it.hasNext()进行检测。若不调用，且下一条记录无效，直接调用it.next()会抛出NoSuchElementException异常。
+//        System.out.println(iterator.next());
+
+        //方式二：不推荐
+//        for(int i = 0;i < coll.size();i++){
+//            System.out.println(iterator.next());
+//        }
+
+        //方式三：推荐
+        while(iterator.hasNext()){// 判断集合里面是否有函数
+            System.out.println(iterator.next());
+        }
+    }
+
+
+    @Test
+    public void test2(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //错误方式一：
+//        Iterator iterator = coll.iterator();
+//        while(iterator.next() != null){
+//            System.out.println(iterator.next());
+//        }
+
+        //错误方式二：
+        //集合对象每次调用iterator()方法都得到一个全新的迭代器对象，默认游标都在集合的第一个元素之前。
+//        while(coll.iterator().hasNext()){
+//            System.out.println(coll.iterator().next());
+//        }
+    }
+
+    //测试Iterator中的remove()方法
+    @Test
+    public void test3(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //删除集合中”Tom”
+        //如果还未调用next()或在上一次调用 next 方法之后已经调用了 remove 方法，
+        // 再调用remove都会报IllegalStateException。
+        Iterator iterator = coll.iterator();
+        while(iterator.hasNext()){
+            Object obj = iterator.next();
+            if("Tom".equals(obj)){
+                iterator.remove();
+            }
+        }
+
+        //遍历集合
+        iterator = coll.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+
+}
