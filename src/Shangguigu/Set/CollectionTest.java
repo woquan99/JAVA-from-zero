72ae2a95d364bea8088794bd677b6847b20684c4
woquan99 <1092999319@qq.com>
2022-08-14 14:59:50 +0800

    2022/8/14 15:00

--- src/Shangguigu/Set/CollectionTest.java
+++ src/Shangguigu/Set/CollectionTest.java
@@ -35,6 +35,7 @@ package Shangguigu.Set;
 
 import org.junit.Test;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
@@ -46,7 +47,7 @@ import java.util.Date;
 public class CollectionTest {
 
     @Test
-    public void test1(){
+    public void test0(){
         Collection coll = new ArrayList();
 
         //add(Object e):将元素e添加到集合coll中
@@ -73,4 +74,87 @@ public class CollectionTest {
         //isEmpty():判断当前集合是否为空
         System.out.println(coll.isEmpty());
     }
+
+
+    /**
+     * Collection接口中声明的方法的测试
+     *
+     * 结论：
+     * 向Collection接口的实现类的对象中添加数据obj时，要求obj所在类要重写equals().
+     */
+    @Test
+    public void test1(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+
+//        Person p = new Person("Jerry",20);
+//        coll.add(p);
+        coll.add(new Person("Jerry",20));
+
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //1.contains(Object obj):判断当前集合中是否包含obj
+        //我们在判断时会调用obj对象所在类的equals()。
+//        boolean contains = coll.contains(123);
+//        System.out.println(contains);
+        System.out.println(coll.contains(new String("Tom")));
+//        System.out.println(coll.contains(p));//true
+        System.out.println(coll.contains(new Person("Jerry",20)));//false -->true
+
+        //2.containsAll(Collection coll1):判断形参coll1中的所有元素是否都存在于当前集合中。
+        Collection coll1 = Arrays.asList(123,4567);
+        System.out.println(coll.containsAll(coll1));
+    }
+
+    @Test
+    public void test2(){
+        //3.remove(Object obj):从当前集合中移除obj元素。
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        // 移除操作
+        coll.remove(1234);
+        System.out.println(coll);
+
+        coll.remove(new Person("Jerry",20));
+        System.out.println(coll);
+
+        //4. removeAll(Collection coll1):差集：从当前集合中移除coll1中所有的元素。
+        Collection coll1 = Arrays.asList(123,456);
+        coll.removeAll(coll1);
+        System.out.println(coll);
+    }
+
+    @Test
+    public void test3(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //5.retainAll(Collection coll1):交集：获取当前集合和coll1集合的交集，并返回给当前集合
+//        Collection coll1 = Arrays.asList(123,456,789);
+//        coll.retainAll(coll1);
+//        System.out.println(coll);
+
+        //6.equals(Object obj):要想返回true，需要当前集合和形参集合的元素都相同。
+        Collection coll1 = new ArrayList();
+        coll1.add(456);
+        coll1.add(123);
+        coll1.add(new Person("Jerry",20));
+        coll1.add(new String("Tom"));
+        coll1.add(false);
+
+        System.out.println(coll.equals(coll1));
+    }
+
+
 }
