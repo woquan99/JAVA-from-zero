f0551f7e9e3529b60358c991a41966be2e65f8e8
woquan99 <1092999319@qq.com>
2022-08-14 11:42:44 +0800

    2022/8/14 11:45

--- src/Shangguigu/Set/CollectionTest.java
+++ src/Shangguigu/Set/CollectionTest.java
@@ -18,6 +18,59 @@ package Shangguigu.Set;
  *
  */
 
+/**
+ *
+ * 二、集合框架
+ *      &---Collection接口：单列集合，用来存储一个一个的对象
+ *          &---List接口：存储有序的、可重复的数据。  -->“动态”数组
+ *              &---ArrayList、LinkedList、Vector
+ *
+ *          &---Set接口：存储无序的、不可重复的数据   -->高中讲的“集合”
+ *              &---HashSet、LinkedHashSet、TreeSet
+ *
+ *      &---Map接口：双列集合，用来存储一对(key - value)一对的数据   -->高中函数：y = f(x)
+ *          &---HashMap、LinkedHashMap、TreeMap、Hashtable、Properties
+ *
+ */
 
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ *
+ * 三、Collection接口中的方法的使用
+ *
+ */
 public class CollectionTest {
+
+    @Test
+    public void test1(){
+        Collection coll = new ArrayList();
+
+        //add(Object e):将元素e添加到集合coll中
+        coll.add("AA");
+        coll.add("BB");
+        coll.add(123);  //自动装箱
+        coll.add(new Date());
+
+        //size():获取添加的元素的个数
+        System.out.println(coll.size());    //4
+
+        //addAll(Collection coll1):将coll1集合中的元素添加到当前的集合中
+        Collection coll1 = new ArrayList();
+        coll1.add(456);
+        coll1.add("CC");
+        coll.addAll(coll1);
+
+        System.out.println(coll.size());    //6
+        System.out.println(coll);
+
+        //clear():清空集合元素
+        coll.clear();
+
+        //isEmpty():判断当前集合是否为空
+        System.out.println(coll.isEmpty());
+    }
 }
